ec7c6d86ca9797dc9fb3a1249045a1cc9cab1bc7
Nithish-CC <nyogaraj@concertcare.com>
2022-02-14 22:17:23 +0530

    feb 14 - route issuse

--- src/config/Routers.tsx
+++ src/config/Routers.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
 import axios from "axios";
 
 import "../config/Axiosconfiq";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { BrowserRouter, Route, Switch, useHistory, RouteProps, Redirect } from "react-router-dom";
+import {BrowserRouter,Route,Switch,useHistory } from 'react-router-dom'
 import Login from "../components/auth/Login";
 import StudentProfileId from "../components/studentdetails/Student";
 import Dashboard from "../components/Dashboard/Dashboard";
@@ -32,105 +32,50 @@ import Placesspotting from "../components/Transport/Placesspotting";
 //     }
 // };
 
-// const PrivateRoute = ({ component: Routers, ...rest }) => {
-//     const token = localStorage.getItem("AccessToken");
-//     <Route {...rest} render={(props) => (token ? <Routers {...props} /> : <Login {...props} />)} />;
-// };
-
-// PrivateRoute.propTypes:any = {
-//     component: PropTypes.object,
-// };
-
-// const Routers = (props: any) => {
-//     if (localStorage.getItem("AccessToken")) {
-//         console.log(localStorage.getItem("AccessToken"));
-//     }
-
-//     return (
-//         <React.Fragment>
-//             <React.StrictMode>
-//                 <BrowserRouter>
-//                     <React.Fragment>
-//                         <Switch>
-//                             <Route exact={true} path="/" component={Login} />
-//                             <PrivateRoute path="/Dashboard" component={Dashboard} />
-//                             <PrivateRoute path="/studentrecord" component={Studentrecord} />
-//                             <PrivateRoute path="/StudentProfileId" component={StudentProfileId} />
-//                             <PrivateRoute path="/Stu_pay" component={Studentpay} />
-//                             <PrivateRoute path="/Stu_add" component={Studentadd} />
-//                             <PrivateRoute path="/Stupro" component={Promotion} />
-//                             <PrivateRoute path="/Stu_fees" component={Yearoffee} />
-//                             <PrivateRoute path="/Fee_master" component={Feemaster} />
-//                             <PrivateRoute path="/Discounttype" component={Discountfee} />
-//                             <PrivateRoute path="/Academicyear" component={Year} />
-//                             <PrivateRoute path="/Grade_section" component={Grade} />
-//                             <PrivateRoute path="/studentprofile" component={StudentProfile} />
-//                             <PrivateRoute path="/StudentprofileSearch" component={StudentprofileSearch} />
-//                             <PrivateRoute path="/placesprice" component={Placesprice} />
-//                             <PrivateRoute path="/placesstoppings" component={Placesspotting} />
-//                             {/* <PrivateRoute exact component={NotFoundScreen} /> */}
-//                         </Switch>
-//                     </React.Fragment>
-//                 </BrowserRouter>
-//             </React.StrictMode>
-//         </React.Fragment>
-//     );
-// };
-
-// const PrivateRoute: React.FC<any> = ({ component: Component, ...rest }) => (
-//     const token = localStorage.getItem("AccessToken");
-//    return <Route {...rest} render={(props) => (token ? <Routers {...props} /> : <Login {...props} />)} />
-// );
-const token = localStorage.getItem("AccessToken");
-const PrivateRoute: React.FC<any> = ({ component: Component, ...rest }) => <Route {...rest} render={(props: any) => (token ? <Routers {...props} /> : <Login {...props} />)} />;
-
-PrivateRoute.propTypes = {
-    component: PropTypes.object,
+const PrivateRoute = (props: any) => {
+	let history = useHistory<any>();
+	const token = localStorage.getItem("AccessToken");
+	if (token) {
+		return <Route exact={true} path={props.path} component={props.component} />;
+	} else {
+		history.push("/");
+		return <Login {...props} />;
+	}
 };
-class Routers extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <React.StrictMode>
-                    <BrowserRouter>
-                        <React.Fragment>
-                        <Switch>
-                                <Route exact={true} path="/" component={Login} />
-                                {token ? <Route path="/Dashboard" component={Dashboard} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/Dashboard" component={Dashboard} /> */}
-                                {token ? <Route path="/studentrecord" component={Studentrecord} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/studentrecord" component={(props: any) => <Studentrecord {...props} />} /> */}
-                                {token ? <Route path="/StudentProfileId" component={StudentProfileId} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/StudentProfileId" component={StudentProfileId} /> */}
-                                {token ? <Route path="/Stu_pay" component={Studentpay} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/Stu_pay" component={Studentpay} /> */}
-                                {token ? <Route path="/Stu_add" component={Studentadd} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/Stu_add" component={Studentadd} /> */}
-                                {token ? <Route path="/Stupro" component={Promotion} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/Stupro" component={Promotion} /> */}
-                                {token ? <Route path="/Stu_fees" component={Yearoffee} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/Stu_fees" component={Yearoffee} /> */}
-                                {token ? <Route path="/Fee_master" component={Feemaster} /> : <Redirect to={"/"} />}
-                                {token ? <Route path="/Discounttype" component={Discountfee} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/Discounttype" component={Discountfee} /> */}
-                                {token ? <Route path="/Academicyear" component={Year} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/Academicyear" component={Year} /> */}
-                                {token ? <Route path="/Grade_section" component={Grade} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/Grade_section" component={Grade} /> */}
-                                {token ? <Route path="/studentprofile" component={StudentProfile} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/studentprofile" component={StudentProfile} /> */}
-                                {token ? <Route path="/StudentprofileSearch" component={StudentprofileSearch} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/StudentprofileSearch" component={StudentprofileSearch} /> */}
-                                {token ? <Route path="/placesprice" component={Placesprice} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/placesprice" component={Placesprice} /> */}
-                                {token ? <Route path="/placesstoppings" component={Placesspotting} /> : <Redirect to={"/"} />}
-                                {/* <PrivateRoute path="/placesstoppings" component={Placesspotting} /> */}
-                            </Switch>
-                        </React.Fragment>
-                    </BrowserRouter>
-                </React.StrictMode>
-            </React.Fragment>
-        );
-    }
-}
+const Routers = (props: any) => {
+	if (localStorage.getItem("AccessToken")) {
+		console.log(localStorage.getItem("AccessToken"));
+	}
+
+	return (
+		<React.Fragment>
+			<React.StrictMode>
+				<BrowserRouter>
+					<React.Fragment>
+						<Switch>
+							<Route exact={true} path="/" component={Login} />
+							<PrivateRoute path="/Dashboard" component={Dashboard} />
+							<PrivateRoute path="/studentrecord" component={Studentrecord} />
+							<PrivateRoute path="/StudentProfileId" component={StudentProfileId} />
+							<PrivateRoute path="/Stu_pay" component={Studentpay} />
+							<PrivateRoute path="/Stu_add" component={Studentadd} />
+							<PrivateRoute path="/Stupro" component={Promotion} />
+							<PrivateRoute path="/Stu_fees" component={Yearoffee} />
+							<PrivateRoute path="/Fee_master" component={Feemaster} />
+							<PrivateRoute path="/Discounttype" component={Discountfee} />
+							<PrivateRoute path="/Academicyear" component={Year} />
+							<PrivateRoute path="/Grade_section" component={Grade} />
+							<PrivateRoute path="/studentprofile" component={StudentProfile} />
+							<PrivateRoute path="/StudentprofileSearch" component={StudentprofileSearch} />
+							<PrivateRoute path="/placesprice" component={Placesprice} />
+							<PrivateRoute path="/placesstoppings" component={Placesspotting} />
+							{/* <PrivateRoute exact component={NotFoundScreen} /> */}
+						</Switch>
+					</React.Fragment>
+				</BrowserRouter>
+			</React.StrictMode>
+		</React.Fragment>
+	);
+};
+
 export default Routers;
