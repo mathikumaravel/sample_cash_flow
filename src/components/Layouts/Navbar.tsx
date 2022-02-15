d454cd8c47d159df1293a50a2ef44dfca071e29e
Nithish-CC <nyogaraj@concertcare.com>
2022-02-15 17:19:28 +0530

    removed additional options

--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -19,11 +19,11 @@ const Navbar = () => {
                         <li className="nav-item dropdown no-arrow">
                             <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span className="mr-2 d-none d-lg-inline text-gray-600 small">ASAIN</span>
+                                <span className="mr-2 d-none d-lg-inline text-gray-600 small">Cash Flow Admin</span>
                             </a>
                             <div className="dropdown-menu dropdown-menu-right shadow animated</h1grow-in"
                                 aria-labelledby="userDropdown">
-                                <a className="dropdown-item" href="#">
+                                {/* <a className="dropdown-item" href="#">
                                     <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
@@ -34,12 +34,12 @@ const Navbar = () => {
                                 <a className="dropdown-item" href="#">
                                     <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Activity Log
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" onClick={()=>{callLogout()}}>
+                                </a> */}
+                                {/* <div className="dropdown-divider"></div> */}
+                                <button className="dropdown-item" onClick={()=>{callLogout()}}>
                                     <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
-                                </a>
+                                </button>
                             </div>
                         </li>
                     </ul>
