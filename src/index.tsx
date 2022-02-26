5049c873459ec222a1dddc420ac388cad1b26cec
Nithish-CC <nyogaraj@concertcare.com>
2022-02-26 15:26:47 +0530

    index

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import reportWebVitals from './reportWebVitals';
-// import Routers from './config/Routers';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Routers from './config/Routers';
 
-// export const baseUrl= "http://localhost:5000/api/v1/"
+export const baseUrl= "http://localhost:5000/api/v1/"
 
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Routers/>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <Routers/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
