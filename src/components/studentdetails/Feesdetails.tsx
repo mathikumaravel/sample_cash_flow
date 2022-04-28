5849b458f14ee152f6fc6534264b480e8aa673db
karthi-Ccare <karthikeyan.arunachalam@concertidc.com>
2022-04-28 14:39:25 +0530

    Build

--- src/components/studentdetails/Feesdetails.tsx
+++ src/components/studentdetails/Feesdetails.tsx
@@ -243,7 +243,7 @@ const Feesdetails = (props: any) => {
                                             {lastFourRecord &&
                                                 lastFourRecord.length &&
                                                 lastFourRecord.map((data: any) => {
-                                                    return <option> {data.amount_paid}</option>;
+                                                    return <option> {data.cum_amt}</option>;
                                                 })}
                                         </label>
                                     </div>
